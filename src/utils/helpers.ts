f58ea495d1696fc443a07ad4adc21025f3eeffe4
sleeply <sheroff01@mail.ru>
2025-04-02 13:34:14 +0500

    awd

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,35 +2,9 @@ import { notify } from "@kyvg/vue3-notification";
 import { AxiosError } from "axios";
 
 export const setError = async (err: AxiosError<any>) => {
-    const response = err.response;
-    const isJsonBlob = (data: any) => data instanceof Blob && data.type === "application/json";
-    const responseData = isJsonBlob(response?.data) ? await response?.data?.text() : response?.data || {};
-    const responseJson = typeof responseData === "string" ? JSON.parse(responseData) : responseData;
-
-    if (responseJson) {
-      const text = await responseJson?.text ? responseJson.text() : responseJson
-      let parsed = JSON.parse(JSON.stringify(text));
-
-      if (parsed && parsed?.errors) {
-        for (const item in parsed?.errors) {
-          notify({
-            type: "error",
-            text: parsed.errors[item]?.toString()
-          });
-        }
-      }
-    }
-  };
-  export const setCustomError = (text: string, type: string) => {
-    if (type == "error") {
-      notify({
-        type: "error",
-        text: text
-      });      
-    } else if (type == "warn") {
-      notify({
-        type: "warn",
-        text: text
-      });          
-    }
-  }
+  const response = err.response;
+  notify({
+    text: response?.data.message,
+    type: "error",
+  });
+};
